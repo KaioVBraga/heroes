3f1b5249c7220f8db93f3d46ea3886a6fc810911
kaiovbraga <kaiovbraga2001@gmail.com>
2022-03-02 23:00:46 -0300

    feat: Cors; Search by name

--- src/main/java/com/heroes/heroes/controller/HeroController.java
+++ src/main/java/com/heroes/heroes/controller/HeroController.java
@@ -1,27 +1,26 @@
 package com.heroes.heroes.controller;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.heroes.heroes.model.Hero;
 import com.heroes.heroes.repository.HeroRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-import javax.persistence.EntityNotFoundException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 @RequestMapping("/heroes")
 public class HeroController {
@@ -29,7 +28,11 @@ public class HeroController {
     private HeroRepository heroRepository;
 
     @GetMapping
-    public List<Hero> index() {
+    public List<Hero> index(@RequestParam(value = "name", required = false) String name) {
+        if (name != null) {
+            return heroRepository.findByName(name);
+        }
+
         return heroRepository.findAll();
     }
 
--- src/main/java/com/heroes/heroes/repository/HeroRepository.java
+++ src/main/java/com/heroes/heroes/repository/HeroRepository.java
@@ -1,12 +1,16 @@
 package com.heroes.heroes.repository;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.heroes.heroes.model.Hero;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HeroRepository extends JpaRepository<Hero, Long> {
-
+    @Query("SELECT h FROM Hero h WHERE h.name like %?1%")
+    List<Hero> findByName(String name);
 }
